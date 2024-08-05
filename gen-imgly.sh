23b0b0d9da8685e7e227ff51c25490fa4c581460
Natchanun Yindeesuk <natchanun.yindeesuk@post.ch>
2024-08-05 09:34:40 +0700

    add script gen imgly typing

--- gen-imgly.sh
+++ gen-imgly.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
-
-# Define the input file
 INFILE=./platforms/android/app/build/outputs/logs/manifest-merger-debug-report.txt
-
-# Read the input file line by line
+startString="]"
+endSting="/AndroidManifest"
+allPath=()
 while read -r LINE
 do
     if [[ $LINE == *"[ly.img"* ]]; then
-        printf '%s\n' "$LINE"
+        subEnd="${LINE%%$endSting*}"
+        subStart="${subEnd%%$startString*}"
+        path=${subEnd:$((${#subStart} + 2))}
+        allPath+=(${path}"/jars")
     fi
 done < "$INFILE"
+
+uniquePath=($(echo "${allPath[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+count=0
+cd typings/android
+for path in "${uniquePath[@]}"
+do
+    printf '%s\n' "$path"
+#    ns typings android --jar "${path}"
+#    (( count++ ))
+#    mv android.d.ts android${count}.d.ts
+done
+
